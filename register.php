2a96840339c9088ec04de5a2c3bf0acb6e44fd58
Thanh Nga <ttnga912@gmail.com>
2019-10-17 00:05:45 +0700

    add login handles

---
+++ register.php
@@ -0,0 +1,204 @@
+<?php
+  // Include config file
+  require_once "config.php";
+
+  // Define variables and initialize with empty values
+  $username = $password = $confirm_password = "";
+  $username_err = $password_err = $confirm_password_err = "";
+
+  // Processing form data when form is submitted
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Validate username
+    if (empty(trim($_POST["username"]))) {
+      $username_err = "Please enter a username.";
+    } else {
+      // Prepare a select statement
+      $sql = "SELECT id FROM users WHERE username = ?";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+        // Set parameters
+        $param_username = trim($_POST["username"]);
+
+        // Attempt to execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+          /* store result */
+          mysqli_stmt_store_result($stmt);
+
+          if (mysqli_stmt_num_rows($stmt) == 1) {
+            $username_err = "This username is already taken.";
+          } else {
+            $username = trim($_POST["username"]);
+          }
+        } else {
+          echo "Oops! Something went wrong. Please try again later.";
+        }
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
+    }
+
+    // Validate password
+    if (empty(trim($_POST["password"]))) {
+      $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+      $password_err = "Password must have atleast 6 characters.";
+    } else {
+      $password = trim($_POST["password"]);
+    }
+
+    // Validate confirm password
+    if (empty(trim($_POST["confirm_password"]))) {
+      $confirm_password_err = "Please confirm password.";
+    } else {
+      $confirm_password = trim($_POST["confirm_password"]);
+      if (empty($password_err) && ($password != $confirm_password)) {
+        $confirm_password_err = "Password did not match.";
+      }
+    }
+
+    // Check input errors before inserting in database
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
+      // Prepare an insert statement
+      $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+        // Set parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+        // Attempt to execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+          // Redirect to login page
+          header("location: login.php");
+        } else {
+          echo "Something went wrong. Please try again later.";
+        }
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Register</title>
+  <style>
+    span.password {
+      float: right;
+      padding-top: 16px;
+    }
+
+    /* The Modal (background) */
+    .modal {
+      background-color: rgba(0, 0, 0, 0.4);
+      /* Black w/ opacity */
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+      margin: 5% auto 15% auto;
+      /* 5% from the top, 15% from the bottom and centered */
+      width: 40%;
+      /* Could be more or less, depending on screen size */
+    }
+
+    /* Add Zoom Animation */
+    .animate {
+      -webkit-animation: animatezoom 0.6s;
+      animation: animatezoom 0.6s
+    }
+
+    @-webkit-keyframes animatezoom {
+      from {
+        -webkit-transform: scale(0)
+      }
+
+      to {
+        -webkit-transform: scale(1)
+      }
+    }
+
+    @keyframes animatezoom {
+      from {
+        transform: scale(0)
+      }
+
+      to {
+        transform: scale(1)
+      }
+    }
+
+    /* Change styles for span and cancel button on extra small screens */
+    @media screen and (max-width: 300px) {
+      span.password {
+        display: block;
+        float: none;
+      }
+
+    }
+  </style>
+</head>
+
+<body>
+  <h4>Register Form</h4>
+<button onclick="document.getElementById('dialog').style.display='block'" class="w3-button w3-green">Register</button>
+  <div id="dialog" class="modal">
+    <form class="modal-content animate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <div class="w3-padding-large">
+        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+          <label for="userName"><b>Username</b></label>
+          <input class="w3-input w3-padding-large" type="text" placeholder="Enter Username" name="username" value="<?php echo $username; ?>" required>
+          <span class="help-block"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+          <label for="password"><b>Password</b></label>
+          <input class="w3-input w3-padding-large" placeholder="Enter Password"  type="password" name="password" class="form-control" value="<?php echo $password; ?>" required>
+          <span class="help-block"><?php echo $password_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+          <label for="password"><b>Confirm Password</b></label>
+          <input class="w3-input w3-padding-large" placeholder="Enter Password"  type="password" name="confirm_password" value="<?php echo $confirm_password; ?>" required>
+          <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <button type="submit" class="w3-button w3-blue w3-block">Register</button>
+        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+      </div>
+      <div class="w3-padding" style="background-color:#f1f1f1">
+        <button type="reset" class="w3-btn w3-red">Reset</button>
+        <button type="button" onclick="document.getElementById('dialog').style.display='none'" class="w3-btn w3-white">Cancel</button>
+      </div>
+    </form>
+  </div>
+  <script>
+    // Get the modal
+    var modal = document.getElementById('dialog');
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  </script>
+</body>
+
+</html>
